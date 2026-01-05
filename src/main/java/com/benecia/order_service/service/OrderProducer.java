9026d10bc57fbe2b0b4b89ac70843963cc652bd7
Donghyeok Lim <jh07050@gmail.com>
2026-01-05 17:45:23 +0900

    feat: Kafka Partitioning 활용하여 재고 동시성 문제 해결

--- src/main/java/com/benecia/order_service/service/OrderProducer.java
+++ src/main/java/com/benecia/order_service/service/OrderProducer.java
@@ -5,6 +5,8 @@ import com.benecia.order_service.event.OrderCreated;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
@@ -18,7 +20,13 @@ public class OrderProducer {
     public void sendOrderCreated(OrderCreated createdDto) {
         try {
             log.info("Sending 'order-created' event: {}", createdDto.orderId());
-            streamBridge.send("orderCreated-out-0", createdDto);
+
+            Message<OrderCreated> message = MessageBuilder
+                    .withPayload(createdDto)
+                    .setHeader(KafkaHeaders.KEY, createdDto.productId().getBytes())
+                    .build();
+
+            streamBridge.send("orderCreated-out-0", message);
         } catch (Exception e) {
             log.error("Failed to send order-created event", e);
         }
@@ -27,7 +35,13 @@ public class OrderProducer {
     public void sendOrderCancelled(OrderCancelled cancelledDto) {
         try {
             log.info("Sending 'order-cancelled' event for recovery: {}", cancelledDto.orderId());
-            streamBridge.send("orderCancelled-out-0", MessageBuilder.withPayload(cancelledDto).build());
+
+            Message<OrderCancelled> message = MessageBuilder
+                    .withPayload(cancelledDto)
+                    .setHeader(KafkaHeaders.KEY, cancelledDto.productId().getBytes())
+                    .build();
+
+            streamBridge.send("orderCancelled-out-0", message);
         } catch (Exception e) {
             log.error("Failed to send order-cancelled event", e);
         }
